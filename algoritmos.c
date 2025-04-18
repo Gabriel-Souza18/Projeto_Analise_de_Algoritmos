885e205783f04a3727810454994d88a202ee7600
José Vitor Santos Alves <josevitorsantosalves00@gmail.com>
2025-04-18 10:24:48 -0300

    algoritmo força bruta e minMovimentos

--- algoritmos.c
+++ algoritmos.c
@@ -1,14 +1,145 @@
 #include "algoritmos.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+// Direções: cima esquerda, cima direita, baixo esquerda, baixo direita
+int direcoes[4][2] = {
+    {-1, -1},
+    {-1, 1},
+    {1, -1},
+    {1, 1}
+};
+
+// Converte coordenadas (linha, coluna) para índice no vetor de estado
+int coordenadaParaIndice(int linha, int coluna, int N, int M) {
+    if ((linha + coluna) % 2 != 0) return -1; // casa não jogável
+    return (linha * M + coluna) / 2;
+}
+
+int getCasa(Tabuleiro *tab, int i, int j) {
+    if (i < 0 || j < 0 || i >= tab->N || j >= tab->M) return -1;
+    if ((i + j) % 2 != 0) return -1;
+    int idx = coordenadaParaIndice(i, j, tab->N, tab->M);
+    return tab->estado[idx];
+}
+
+void setCasa(Tabuleiro *tab, int i, int j, int valor) {
+    int idx = coordenadaParaIndice(i, j, tab->N, tab->M);
+    if (idx >= 0) tab->estado[idx] = valor;
+}
+
+// FORÇA BRUTA - com recursão
+int maxCapturas(Tabuleiro *tab, int i, int j) {
+    int maxCaptura = 0;
+
+    for (int d = 0; d < 4; d++) {
+        int mi = i + direcoes[d][0];
+        int mj = j + direcoes[d][1];
+        int ni = i + 2 * direcoes[d][0];
+        int nj = j + 2 * direcoes[d][1];
+
+        if (getCasa(tab, mi, mj) == 2 && getCasa(tab, ni, nj) == 0) {
+            setCasa(tab, i, j, 0);
+            setCasa(tab, mi, mj, 0);
+            setCasa(tab, ni, nj, 1);
+
+            int capturas = 1 + maxCapturas(tab, ni, nj);
+            maxCaptura = MAX(maxCaptura, capturas);
+
+            setCasa(tab, i, j, 1);
+            setCasa(tab, mi, mj, 2);
+            setCasa(tab, ni, nj, 0);
+        }
+    }
+
+    return maxCaptura;
+}
 
 char* forcaBruta(Tabuleiro* tabuleiro) {
-    char*resultado = "Teste forca bruta\n";
-    // Implementação do algoritmo de força bruta
-    // ...
-    return resultado;
+    int maxTotal = 0;
+
+    for (int i = 0; i < tabuleiro->N; i++) {
+        for (int j = 0; j < tabuleiro->M; j++) {
+            if (getCasa(tabuleiro, i, j) == 1) {
+                int capturas = maxCapturas(tabuleiro, i, j);
+                if (capturas > maxTotal) maxTotal = capturas;
+            }
+        }
+    }
+
+    char *res = malloc(16);
+    sprintf(res, "%d\n", maxTotal);
+    return res;
 }
+
+// MÍNIMOS MOVIMENTOS - com pilha iterativa
 char* minMovimentos(Tabuleiro* tabuleiro) {
-    char* resultado = "Teste min movimentos\n";
-    // Implementação do algoritmo de mínimos movimentos
-    // ...
-    return resultado ;
+    int maxCaptura = 0;
+
+    typedef struct {
+        int i, j;
+        int capturas;
+        int* estadoCopia;
+    } Estado;
+
+    int tamanho = (tabuleiro->N * tabuleiro->M + 1) / 2;
+
+    for (int i = 0; i < tabuleiro->N; i++) {
+        for (int j = 0; j < tabuleiro->M; j++) {
+            if (getCasa(tabuleiro, i, j) != 1) continue;
+
+            Estado pilha[500];
+            int topo = 0;
+
+            int* estadoInicial = malloc(sizeof(int) * tamanho);
+            memcpy(estadoInicial, tabuleiro->estado, sizeof(int) * tamanho);
+
+            pilha[topo++] = (Estado){i, j, 0, estadoInicial};
+
+            while (topo > 0) {
+                Estado atual = pilha[--topo];
+                int capturou = 0;
+
+                for (int d = 0; d < 4; d++) {
+                    int mi = atual.i + direcoes[d][0];
+                    int mj = atual.j + direcoes[d][1];
+                    int ni = atual.i + 2 * direcoes[d][0];
+                    int nj = atual.j + 2 * direcoes[d][1];
+
+                    Tabuleiro copia;
+                    copia.N = tabuleiro->N;
+                    copia.M = tabuleiro->M;
+                    copia.estado = malloc(sizeof(int) * tamanho);
+                    memcpy(copia.estado, atual.estadoCopia, sizeof(int) * tamanho);
+
+                    if (getCasa(&copia, mi, mj) == 2 && getCasa(&copia, ni, nj) == 0) {
+                        setCasa(&copia, atual.i, atual.j, 0);
+                        setCasa(&copia, mi, mj, 0);
+                        setCasa(&copia, ni, nj, 1);
+
+                        int* novaCopia = malloc(sizeof(int) * tamanho);
+                        memcpy(novaCopia, copia.estado, sizeof(int) * tamanho);
+
+                        pilha[topo++] = (Estado){ni, nj, atual.capturas + 1, novaCopia};
+                        capturou = 1;
+                    }
+
+                    free(copia.estado);
+                }
+
+                if (!capturou && atual.capturas > maxCaptura) {
+                    maxCaptura = atual.capturas;
+                }
+
+                free(atual.estadoCopia);
+            }
+        }
+    }
+
+    char *res = malloc(16);
+    sprintf(res, "%d\n", maxCaptura);
+    return res;
 }
--- teste.c
+++ teste.c
@@ -2,22 +2,28 @@
 #include "io.h"
 #include "algoritmos.h"
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
+int main(int argc, char *argv[]) {
+    if (argc != 3 || strcmp(argv[1], "-i") != 0) {
+        printf("Uso correto: %s -i entrada.txt\n", argv[0]);
+        return 1;
+    }
 
-
-int main() {
-    char *entrada = lerEntrada("entrada.txt");
+    char *entrada = lerEntrada(argv[2]);
     if (!entrada) return 1;
 
-    char *ptr = entrada;
-    char saida[2048] = "";
+    struct timeval inicio, fim;
+    struct rusage uso;
 
-    Tabuleiro **Tabuleiros = NULL;
-    int numTabuleiros = 0;
+    gettimeofday(&inicio, NULL);
+
+    char *ptr = entrada;
+    char saida[4096] = "";
 
     while (1) {
         int N, M;
@@ -25,24 +31,26 @@ int main() {
         if (N == 0 && M == 0) break;
 
         Tabuleiro *tab = inicializaTabuleiro(&ptr);
-        imprimeTabuleiro(tab);
 
-        Tabuleiros = realloc(Tabuleiros, (numTabuleiros+1)*sizeof *Tabuleiros);
-        Tabuleiros[numTabuleiros++] = tab;
+        // Escolha aqui qual função usar
+        char *resultado = forcaBruta(tab);           // ou minMovimentos(tab)
+        strcat(saida, resultado);
+        free(resultado);
 
-        strcat(saida, "0\n"); // valor fictício por enquanto
-        printf("%s", saida);
+        liberaTabuleiro(tab);
     }
 
-    printf("%s", forcaBruta(Tabuleiros[0]));
-    printf("%s", minMovimentos(Tabuleiros[0]));
+    gettimeofday(&fim, NULL);
+    getrusage(RUSAGE_SELF, &uso);
 
-
-    for (int i = 0; i < numTabuleiros; i++) {
-        liberaTabuleiro(Tabuleiros[i]);
-    }
-    free(Tabuleiros);
     escreverSaida("saida.txt", saida);
+
+    long tempoUsuario = uso.ru_utime.tv_sec * 1000000 + uso.ru_utime.tv_usec;
+    long tempoSistema = uso.ru_stime.tv_sec * 1000000 + uso.ru_stime.tv_usec;
+
+    printf("Tempo de usuário: %ld us\n", tempoUsuario);
+    printf("Tempo de sistema: %ld us\n", tempoSistema);
+
     free(entrada);
     return 0;
 }
