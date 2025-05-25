10bde0c4ee36ed08669f67fd20ad9ad67d3b1f74
Gabriel Souza <gabrielsisou@gmail.com>
2025-05-25 09:58:44 -0300

    Testando algoritmos

--- TP2/Solucao.h
+++ TP2/Solucao.h
@@ -29,6 +29,10 @@ Resultado* resolverComPD(Povos *povos, Caminhos *caminhos, int D, int W);
 //Resolve levando a distancia em consideração
 Resultado* resolverComDistancia(Povos *povos, Caminhos *caminhos, int D, int W);
 
+Grafo* criarGrafo(Caminhos *caminhos, int numPovos);
+void adicionarAresta(Grafo *grafo, int origem, int destino, int distancia);
+void liberarGrafo(Grafo *grafo);
+void dfs(Povos *povos, Grafo *grafo, int atual, int Drestante, int *visitados, int *melhorHabilidade, Resultado **melhorRes, int W);
 // Libera memória da estrutura Resultado
 void liberarResultado(Resultado *res);
 
